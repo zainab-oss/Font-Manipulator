7d43db873c853d92d7b65bf2c9b474d0d2aefea9
Serious Zainab <85008588+zainab-oss@users.noreply.github.com>
2023-02-18 17:55:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+leftWristX = 0;
+rightWrist = 0;
+difference = 0;
+
 function setup() 
 {
     video = createCapture(VIDEO);
@@ -7,7 +11,7 @@ function setup()
     canvas.position(600,150);
 
     poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('Pose',gotPoses)
+    poseNet.on('pose', gotPoses);
 
 }
 
@@ -16,15 +20,32 @@ function modelLoaded()
     console.log("poseNet has been initialized");
 }
 
-function draw()
-{
-    background("#E37383");
-}
 
-function gotPoses()
+
+function gotPoses(results)
 {
-    if (results > 0)
+    if (results.length > 0)
     {
         console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWrist = " + leftWristX + "rightWrist = " + rightWristX + "difference" + difference);
     }
 }
+
+function draw() {
+    background('#E37383');
+    
+      document.getElementById("font_size").innerHTML = "Font size of the text will be = " + difference +"px";
+    textSize(difference);
+    fill('#FFDAB9');
+    text('Zainab', 50, 400);
+    }
+    
+
+
+
+
+
